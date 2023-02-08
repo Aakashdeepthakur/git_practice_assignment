be937ab5a22bb3575b1184474f10a7b304c9838d
Aakash Deep Thakur <aakash.thakur2396@gmail.com>
2023-02-08 14:10:20 +0530

    I have written the prime number code

---
+++ prime.js
@@ -0,0 +1,14 @@
+let number= 13;
+let count=0;
+
+for (let i=1;i<=number;i++){
+if(number%2==0){
+count++;
+}
+}
+if(count==2){
+console.log("it is a prime number");
+}
+else{
+console.log("it is not a prime number");
+}
